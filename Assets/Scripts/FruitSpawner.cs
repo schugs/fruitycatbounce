c3178ae08a5e4c9955576e41b0f23e0fdd04ac68
schugar5 <schugar5@msu.edu>
2018-05-06 00:28:15 -0700

    falling fruit

---
+++ Assets/Scripts/FruitSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour {
+
+    const float FRUIT_SPAWN_HEIGHT = 10f;
+    const float FRUIT_SPAWN_DELAY = 5f;
+    const float FRUIT_SPAWN_RANGE = 5f;
+
+    public GameObject fruitPrefab;
+
+    private float timer = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if (timer > FRUIT_SPAWN_DELAY)
+        {
+            SpawnFruit();
+            timer = 0f;
+        }
+	}
+
+    private void SpawnFruit(){
+        GameObject fruit = (GameObject)Instantiate(fruitPrefab);
+
+        float xPos = Random.Range(-FRUIT_SPAWN_RANGE, FRUIT_SPAWN_RANGE);
+        fruit.transform.position = new Vector3(xPos, FRUIT_SPAWN_HEIGHT, 0f);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    const float PLAYER_SPEED_MULTIPLIER = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xMovement = Input.GetAxis("Horizontal");
+
+        this.gameObject.transform.position += new Vector3(xMovement, 0f, 0f)
+            * PLAYER_SPEED_MULTIPLIER * Time.deltaTime;
+	}
+}
